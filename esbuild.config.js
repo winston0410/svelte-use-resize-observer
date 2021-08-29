e4af31d5b9f9b5b664a95347f172b0c650cd7d34
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-29 22:20:00 +0100

    Fix main and format

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,8 @@
-require('esbuild').build({
+import esbuild from 'esbuild'
+
+esbuild.build({
   entryPoints: ['index.ts'],
   bundle: true,
+  format: "esm",
   outfile: './dist/index.js',
 }).catch(() => process.exit(1))
